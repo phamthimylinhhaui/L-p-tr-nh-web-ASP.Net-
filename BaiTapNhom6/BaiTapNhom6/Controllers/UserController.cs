cd00b419a10a25c8800c8b738b1ff36906c6143c
linh <phamthimylinhhaui@.com>
2021-11-05 17:39:15 +0700

    ghep layout va ket noi csdl

---
+++ BaiTapNhom6/BaiTapNhom6/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTapNhom6.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        //login user
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult register()
+        {
+            return View();
+        }
+        //acount
+        public ActionResult account()
+        {
+            return View();
+        }
+    }
+}
